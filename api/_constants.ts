5f57d3c5282e7e558970b360635a32b8a536e182
Nate Wagner <nate.o.wagner@gmail.com>
2023-06-02 15:46:33 -0400

    Rearrange aws serverless functions

--- api/_constants.ts
+++ api/_constants.ts
@@ -1,11 +1,12 @@
 export const USER_TOKEN = "user-token";
 
-const JWT_SECRET_KEY: string | undefined = process.env.JWT_SECRET_KEY!;
-
 export function getJwtSecretKey(): string {
-  if (!JWT_SECRET_KEY || JWT_SECRET_KEY.length === 0) {
-    throw new Error("The environment variable JWT_SECRET_KEY is not set.");
-  }
+  return getEnvVar("JWT_SECRET_KEY");
+}
 
-  return JWT_SECRET_KEY;
+export function getEnvVar(variableName: string): string {
+  const environmentVariable = process.env[variableName];
+  if (environmentVariable === undefined || environmentVariable.length === 0)
+    throw new Error(`The environment variable ${variableName} is not set.`);
+  return environmentVariable;
 }
