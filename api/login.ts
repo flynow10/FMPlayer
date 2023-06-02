fc1e12e93fa0e8cc43cf1ab59f1e58572ad66b58
Nate Wagner <nate.o.wagner@gmail.com>
2023-06-01 21:55:03 -0400

    Add authentication middleware (Still not hooked up to library)

---
+++ api/login.ts
@@ -0,0 +1,22 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import { setUserCookie } from "./_auth.js";
+
+function checkAuthorization(request: VercelRequest): boolean {
+  const hash = request.query.p,
+    correctHash = process.env.PASSWORD_HASH;
+  if (!correctHash) {
+    throw Error("Missing correct hash variable!");
+  }
+  if (!hash || typeof hash !== "string") {
+    return false;
+  }
+
+  return hash === correctHash;
+}
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  if (checkAuthorization(req)) {
+    return (await setUserCookie(res)).json({ success: true });
+  }
+  return res.status(401).json({ success: false });
+}
--- api/upload.ts
+++ api/upload.ts
@@ -1,12 +1,8 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import { createPresignedPost } from "@aws-sdk/s3-presigned-post";
 import { S3Client } from "@aws-sdk/client-s3";
-import { checkAuthorization } from "./_checkAuth.mjs";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  if (!checkAuthorization(req)) {
-    return res.status(401).json("Failed to authenticate request");
-  }
   const { S3_BUCKET_NAME, AWS_KEY, AWS_SECRET } = process.env;
   if (!S3_BUCKET_NAME || !AWS_KEY || !AWS_SECRET) {
     console.log(S3_BUCKET_NAME);
