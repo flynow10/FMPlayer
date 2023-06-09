ef0bbe6c545fd1bc34b76bc4151c771d9e80346c
Nate Wagner <nate.o.wagner@gmail.com>
2023-06-08 21:25:17 -0400

    Refactor song list columns and add db based sort functionality

---
+++ src/components/pages/LoadingPages.tsx
@@ -0,0 +1,21 @@
+import { Loader2 } from "lucide-react";
+
+export function FullCover() {
+  return (
+    <div className="relative w-full h-full">
+      <h1 className="text-3xl absolute -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
+        Loading <Loader2 className="inline animate-spin" />
+      </h1>
+    </div>
+  );
+}
+
+export function Blur() {
+  return (
+    <div className="absolute top-0 left-0 w-full h-full backdrop-blur">
+      <h1 className="text-3xl absolute -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
+        Loading <Loader2 className="inline animate-spin" />
+      </h1>
+    </div>
+  );
+}
