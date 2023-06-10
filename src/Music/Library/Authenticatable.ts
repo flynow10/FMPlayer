6c843d1ec439dcd14e9364c75d6bc859a07b9695
Nate Wagner <nate.o.wagner@gmail.com>
2023-06-10 18:52:43 -0400

    Remove pass hash localstorage; switched to using cookies

--- src/Music/Library/Authenticatable.ts
+++ src/Music/Library/Authenticatable.ts
@@ -1,10 +1,9 @@
 export interface Authenticatable {
   isAuthenticated(): Promise<boolean>;
-  tryLoadAuth(): Promise<boolean>;
   authenticate(key: string): Promise<boolean>;
 }
 
 export function isAuthenticatable(object: any): object is Authenticatable {
-  const props = ["isAuthenticated", "tryLoadAuth", "authenticate"];
+  const props = ["isAuthenticated", "authenticate"];
   return props.every((prop) => prop in object);
 }
