92a43edfc738d4f6590e580e153223bd80dd7c41
Nate Wagner <nate.o.wagner@gmail.com>
2023-06-23 15:55:13 -0400

    build: add vite chunking plugin to split up build assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
+import { chunkSplitPlugin } from "vite-plugin-chunk-split";
 
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), chunkSplitPlugin()],
   define: {
     __APP_ENV__: JSON.stringify(
       process.env.VITE_VERCEL_ENV ?? process.env.VERCEL_ENV
