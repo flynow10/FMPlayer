4e9b0e1cf07192e3c71211654a5cfc45c4d56fcc
Nate Wagner <nate.o.wagner@gmail.com>
2023-06-25 00:37:44 -0400

    style: change eslint command to use ./api and ./api-lib files aswell

--- api/ably-auth.ts
+++ api/ably-auth.ts
@@ -29,6 +29,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== "GET") {
     return res.status(405).json({ error: "Method not allowed" });
   }
+
   try {
     const token = await getAblyToken();
     res.status(200).json(token);
--- api/login.ts
+++ api/login.ts
@@ -4,9 +4,11 @@ import { setUserCookie } from "../api-lib/auth.js";
 function checkAuthorization(request: VercelRequest): boolean {
   const hash = request.body.hash,
     correctHash = process.env.PASSWORD_HASH;
+
   if (!correctHash) {
     throw Error("Missing correct hash variable!");
   }
+
   if (!hash || typeof hash !== "string") {
     return false;
   }
@@ -20,8 +22,10 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
       .status(405)
       .json({ success: false, error: "Method not allowed" });
   }
+
   if (checkAuthorization(req)) {
     return (await setUserCookie(res)).status(200).json({ success: true });
   }
+
   return res.status(200).json({ success: false, error: "Invalid password" });
 }
