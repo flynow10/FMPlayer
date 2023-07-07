c7aa64b3a22506eeac2b2d0c1b3aa90104ce9c95
Nate Wagner <nate.o.wagner@gmail.com>
2023-07-06 21:21:02 -0400

    ci: update prisma datasources and migrations to work new env vars

--- prisma/migrations/20230706222322_init/migration.sql
+++ prisma/migrations/20230706222322_init/migration.sql
@@ -2,13 +2,14 @@
 CREATE TABLE "Song" (
     "id" UUID NOT NULL,
     "title" VARCHAR(256) NOT NULL,
-    "artists" VARCHAR(64)[] DEFAULT ARRAY[]::VARCHAR(64)[],
+    "artists" VARCHAR(64)[] DEFAULT ARRAY['Unknown Artist']::VARCHAR(64)[],
     "featuring" VARCHAR(64)[] DEFAULT ARRAY[]::VARCHAR(64)[],
     "genre" VARCHAR(64) NOT NULL DEFAULT 'Unknown',
     "albumId" UUID,
     "trackNumber" INTEGER NOT NULL DEFAULT 1,
     "modifiedOn" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "createdOn" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "audioUploaded" TIMESTAMP(3),
 
     CONSTRAINT "Song_pkey" PRIMARY KEY ("id")
 );
@@ -17,7 +18,7 @@ CREATE TABLE "Song" (
 CREATE TABLE "Album" (
     "id" UUID NOT NULL,
     "title" VARCHAR(256) NOT NULL,
-    "artists" VARCHAR(64)[] DEFAULT ARRAY[]::VARCHAR(64)[],
+    "artists" VARCHAR(64)[] DEFAULT ARRAY['Unknown Artist']::VARCHAR(64)[],
     "featuring" VARCHAR(64)[] DEFAULT ARRAY[]::VARCHAR(64)[],
     "genre" VARCHAR(64) NOT NULL DEFAULT 'Unknown',
     "modifiedOn" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
