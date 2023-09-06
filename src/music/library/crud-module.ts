a7ab4e26db8456da13332ca9bcc8972d6686c90f
Nate Wagner <nate.o.wagner@gmail.com>
2023-09-06 19:29:13 -0400

    fix: dates returned from the database were string instead of objects

--- src/music/library/crud-module.ts
+++ src/music/library/crud-module.ts
@@ -37,16 +37,64 @@ export type CRUDObjects = {
   [Key in keyof Music.DB.TableTypes as Uncapitalize<Key>]: CRUDMethods<Key>;
 };
 
+// https://stackoverflow.com/questions/72190916/replace-a-specific-type-with-another-type-in-a-nested-object-typescript
+type ReplaceTypes<ObjType extends object, FromType, ToType> = {
+  [KeyType in keyof ObjType]: ObjType[KeyType] extends object
+    ? ReplaceTypes<ObjType[KeyType], FromType, ToType> // Recurse
+    : ObjType[KeyType] extends FromType // Not recursing, need to change?
+    ? ToType // Yes, change it
+    : ObjType[KeyType]; // No, keep original
+};
+
+function fixJsonDateStrings<TypeWithDate extends object>(
+  input: ReplaceTypes<TypeWithDate, Date, string>
+): TypeWithDate {
+  const output: Partial<TypeWithDate> = {};
+  (
+    Object.entries(input) as Array<
+      [
+        keyof TypeWithDate,
+        ReplaceTypes<TypeWithDate, Date, string>[keyof TypeWithDate]
+      ]
+    >
+  ).forEach(([key, value]) => {
+    if (!value) {
+      output[key] = value as TypeWithDate[typeof key];
+      return;
+    }
+    if (Array.isArray(value)) {
+      output[key] = value.map((value) =>
+        fixJsonDateStrings(value)
+      ) as TypeWithDate[typeof key];
+      return;
+    }
+    if (typeof value === "object") {
+      output[key] = fixJsonDateStrings<typeof value>(
+        value as ReplaceTypes<typeof value, Date, string>
+      ) as TypeWithDate[typeof key];
+      return;
+    }
+    if (typeof value === "string") {
+      if (!Number.isNaN(Date.parse(value))) {
+        output[key] = new Date(value) as TypeWithDate[typeof key];
+        return;
+      }
+    }
+    output[key] = value as TypeWithDate[typeof key];
+  });
+  return output as TypeWithDate;
+}
+
 function getMethodFactory<T extends keyof Music.DB.TableTypes>(
   table: T
 ): GetMethod<T> {
   return async function (where) {
-    return await VercelAPI.makeRequest(
-      Endpoint.DB,
-      { ...where },
-      table,
-      "POST"
-    );
+    const request: ReplaceTypes<Music.DB.TableTypes[T], Date, string> | null =
+      await VercelAPI.makeRequest(Endpoint.DB, { ...where }, table, "POST");
+    if (request === null) {
+      return null;
+    }
+    return fixJsonDateStrings(request);
   };
 }
 
@@ -54,7 +102,13 @@ function createMethodFactory<T extends keyof Music.DB.TableTypes>(
   table: T
 ): CreateMethod<T> {
   return async function (data) {
-    return await VercelAPI.makeRequest(Endpoint.DB, { ...data }, table, "PUT");
+    const request: ReplaceTypes<Music.DB.TableTypes[T], Date, string> | null =
+      await VercelAPI.makeRequest(Endpoint.DB, { ...data }, table, "PUT");
+
+    if (request === null) {
+      return null;
+    }
+    return fixJsonDateStrings(request);
   };
 }
 
@@ -62,12 +116,14 @@ function updateMethodFactory<T extends keyof Music.DB.TableTypes>(
   table: T
 ): UpdateMethod<T> {
   return async function (where, data) {
-    return await VercelAPI.makeRequest(
-      Endpoint.DB,
-      { where, data },
-      table,
-      "PATCH"
-    );
+    const request: ReplaceTypes<Music.DB.TableTypes[T], Date, string> | null =
+      await VercelAPI.makeRequest(Endpoint.DB, { where, data }, table, "PATCH");
+
+    if (request === null) {
+      return null;
+    }
+
+    return fixJsonDateStrings(request);
   };
 }
 
@@ -75,12 +131,12 @@ function deleteMethodFactory<T extends keyof Music.DB.TableTypes>(
   table: T
 ): DeleteMethod<T> {
   return async function (where) {
-    return await VercelAPI.makeRequest(
-      Endpoint.DB,
-      { ...where },
-      table,
-      "DELETE"
-    );
+    const request: ReplaceTypes<Music.DB.TableTypes[T], Date, string> | null =
+      await VercelAPI.makeRequest(Endpoint.DB, { ...where }, table, "DELETE");
+    if (request === null) {
+      return null;
+    }
+    return fixJsonDateStrings(request);
   };
 }
 
@@ -88,13 +144,9 @@ function listMethodFactory<T extends keyof Music.DB.TableTypes>(
   table: T
 ): ListMethod<T> {
   return async function (where = {}) {
-    return await VercelAPI.makeRequest(
-      Endpoint.DB,
-      { ...where },
-      table,
-      "GET",
-      []
-    );
+    return (
+      await VercelAPI.makeRequest(Endpoint.DB, { ...where }, table, "GET", [])
+    ).map((value) => fixJsonDateStrings(value));
   };
 }
 
