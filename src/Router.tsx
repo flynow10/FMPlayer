900a9b9a43598253d92140e1c6d182b452b6f5d0
Nate Wagner <nate.o.wagner@gmail.com>
2023-09-06 18:21:29 -0400

    fix: fixed loading screen flashing bug

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,17 +3,21 @@ import { VercelAPI } from "@/src/api/vercel-API";
 import { YoutubeAPI } from "@/src/api/youtube-API";
 import App from "@/src/components/App";
 import { Login } from "@/src/components/auth/Login";
+import { FullCover } from "@/src/components/utils/loading-pages/FullCover";
+import { Suspense } from "react";
 import {
+  Await,
   createBrowserRouter,
   defer,
   redirect,
   RouterProvider,
+  useLoaderData,
 } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <AppWrapper />,
 
     loader: async () => {
       if (!(await VercelAPI.isLoggedIn())) {
@@ -36,4 +40,19 @@ const router = createBrowserRouter([
 
 export default function Router() {
   return <RouterProvider router={router} />;
+}
+
+// eslint-disable-next-line react/no-multi-comp
+function AppWrapper() {
+  const routerLoadingData = useLoaderData() as {
+    [key: string]: Promise<unknown>;
+  };
+
+  return (
+    <Suspense fallback={<FullCover />}>
+      <Await resolve={Promise.all(Object.values(routerLoadingData))}>
+        <App />
+      </Await>
+    </Suspense>
+  );
 }
