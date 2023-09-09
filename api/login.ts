21d23ce5a39f3bfb83fb58b46562847964e89004
Nate Wagner <nate.o.wagner@gmail.com>
2023-09-09 12:13:37 -0400

    fix: .env.example included comments on the same line as a var
    Apparently vercel's .env parser can't handle comments on the same line as a variable
    Who knew.

--- api/login.ts
+++ api/login.ts
@@ -1,28 +1,20 @@
 import { handleRequest } from "../api-lib/api-utils.js";
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { setUserCookie } from "../api-lib/auth.js";
-
-function checkAuthorization(hash: string): boolean {
-  const correctHash = process.env.PASSWORD_HASH;
-
-  if (!correctHash) {
-    throw Error("Missing correct hash variable!");
-  }
-
-  return hash === correctHash;
-}
+import { getEnvVar } from "@/api-lib/constants.js";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const requestParams = handleRequest(req, res, {
     expectedBodyParams: ["hash"],
     allowedMethods: "POST",
   });
+  const correctHash = getEnvVar("PASSWORD_HASH");
 
   if (requestParams === null) {
     return;
   }
 
-  if (checkAuthorization(requestParams.body.hash as string)) {
+  if (requestParams.body.hash === correctHash) {
     return (await setUserCookie(res)).status(200).json({ success: true });
   }
 
