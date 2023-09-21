6db346a3c84ea78d0568a781843a039d764bbca0
Nate Wagner <nate.o.wagner@gmail.com>
2023-09-21 18:48:52 -0400

    style: add no results found message to search page

--- src/components/pages/SearchResults.tsx
+++ src/components/pages/SearchResults.tsx
@@ -44,50 +44,58 @@ export default function SearchResults(props: SearchResultsProps) {
       </div>
     );
   };
+
+  const topResultsList = (
+    <div className="flex flex-col p-4">
+      <span className="text-2xl pb-4">Top Results</span>
+      <div className="flex flex-row gap-4">
+        {searchResults.results
+          .filter((result) =>
+            ["track", "album", "artist", "playlist"].includes(result.type)
+          )
+          .slice(0, 3)
+          .map((result) => {
+            const data = searchResults[
+              (result.type + "s") as
+                | "tracks"
+                | "albums"
+                | "artists"
+                | "playlists"
+            ].find((media) => result.id === media.id);
+            if (data === undefined) {
+              throw new Error(
+                "Couldn't find data for media card in search results!"
+              );
+            }
+            return (
+              <MediaCard
+                key={result.id}
+                style="tab-card"
+                type={result.type as DisplayableMediaType}
+                data={data}
+                shouldDisplayType={true}
+              />
+            );
+          })}
+      </div>
+    </div>
+  );
   return (
     <div className="flex flex-col">
       <span className="px-4 pt-4 pb-2 text-gray-400">
         Showing results for: &quot;
         <span className="text-black">{props.searchString}</span>&quot;
       </span>
-      <div className="flex flex-col p-4">
-        <span className="text-2xl pb-4">Top Results</span>
-        <div className="flex flex-row gap-4">
-          {searchResults.results
-            .filter((result) =>
-              ["track", "album", "artist", "playlist"].includes(result.type)
-            )
-            .slice(0, 3)
-            .map((result) => {
-              const data = searchResults[
-                (result.type + "s") as
-                  | "tracks"
-                  | "albums"
-                  | "artists"
-                  | "playlists"
-              ].find((media) => result.id === media.id);
-              if (data === undefined) {
-                throw new Error(
-                  "Couldn't find data for media card in search results!"
-                );
-              }
-              return (
-                <MediaCard
-                  key={result.id}
-                  style="tab-card"
-                  type={result.type as DisplayableMediaType}
-                  data={data}
-                  shouldDisplayType={true}
-                />
-              );
-            })}
-        </div>
-      </div>
+      {searchResults.results.length > 0 ? (
+        topResultsList
+      ) : (
+        <span className="text-2xl p-4">No results found!</span>
+      )}
       {searchResults.albums.length > 0 && createMediaDisplay("albums")}
       {searchResults.tracks.length > 0 && createMediaDisplay("tracks")}
       {searchResults.artists.length > 0 && createMediaDisplay("artists")}
       {/* <pre>{JSON.stringify(searchResults, null, 2)}</pre> */}
-      <span className="px-4 py-8">
+      <span className="px-4 pb-8 pt-2">
         Can&apos;t find what you&apos;re looking for?{" "}
         <a
           onClick={() => {
