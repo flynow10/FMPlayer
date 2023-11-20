43f846268da4af7a0ae3c687148a57cd6bb9c241
Nate Wagner <nate.o.wagner@gmail.com>
2023-11-19 20:08:19 -0500

    feat: add editable title to playlist editor

---
+++ src/components/utils/input-extensions/TogglableInput.tsx
@@ -0,0 +1,63 @@
+import { Edit } from "lucide-react";
+import { useEffect, useState } from "react";
+
+type TogglableInputProps = Omit<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  "value" | "onChange"
+> & {
+  containerProps?: React.HTMLAttributes<HTMLDivElement>;
+  value: string;
+  onChange: (newValue: string) => void;
+  onToggleEdit?: (isEditing: boolean) => void;
+};
+
+export default function TogglableInput(props: TogglableInputProps) {
+  const [editing, setEditing] = useState(false);
+
+  const { value, containerProps, onChange, onToggleEdit, ...inputProps } =
+    props;
+
+  useEffect(() => {
+    onToggleEdit?.(editing);
+  }, [editing, onToggleEdit]);
+
+  return (
+    <div className="flex gap-2" {...containerProps}>
+      {editing ? (
+        <input
+          className="grow border-2 p-1 rounded-md"
+          {...inputProps}
+          value={value}
+          onChange={(event) => {
+            onChange(event.target.value);
+          }}
+          onBlur={() => {
+            setEditing(false);
+          }}
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              setEditing(false);
+            }
+          }}
+        />
+      ) : (
+        <span
+          onClick={() => {
+            setEditing(true);
+          }}
+          className={inputProps.className ?? "grow p-1"}
+        >
+          {value}
+        </span>
+      )}
+      <button
+        className="hover:bg-gray-200 rounded-md p-1 ml-auto"
+        onClick={() => {
+          setEditing(!editing);
+        }}
+      >
+        <Edit />
+      </button>
+    </div>
+  );
+}
