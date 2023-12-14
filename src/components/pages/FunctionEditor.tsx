fca7333c2e2f7b414048f52340e9cc26a4de1f10
Nate Wagner <nate.o.wagner@gmail.com>
2023-12-13 19:04:41 -0500

    feat: add saving functionality to function editor

--- src/components/pages/FunctionEditor.tsx
+++ src/components/pages/FunctionEditor.tsx
@@ -9,40 +9,154 @@ import { Functions } from "@/src/types/functions";
 import CodeMirror from "@uiw/react-codemirror";
 import classNames from "classnames";
 import { Braces, Save, TextQuote, X } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { tokyoNight } from "@uiw/codemirror-theme-tokyo-night";
 // import { simpleLezerLinter } from "@/src/music/functions/codemirror/simple-lezer-linter";
 // import { codeFolding } from "@codemirror/language";
 // import { FMLanguage } from "@/src/music/functions/codemirror/fm-language";
 import ActionOverlay from "@/src/components/functions/drag-overlays/ActionOverlay";
 import NumberOverlay from "@/src/components/functions/drag-overlays/NumberOverlay";
-import { createEmpty } from "@/src/music/functions/utils/create-empty";
 import { validateFunction } from "@/src/music/functions/validation/validate-function";
+import { usePageContext } from "@/src/contexts/PageContext";
+import { MusicLibrary } from "@/src/music/library/music-library";
+import { Music } from "@/src/types/music";
+import { toast } from "react-toastify";
+import { Prisma } from "@prisma/client";
 
 type DisplayMode = "text" | "blocks";
 
 export default function FunctionEditor() {
-  const [functionTree, setFunctionTree] = useState<Functions.FunctionTree>(
-    () => [createEmpty.play(), createEmpty.loop()]
-  );
+  const pages = usePageContext();
+  const { isNew, id: oldFunctionId } = pages.data as
+    | { isNew: false; id: string }
+    | { isNew: true; id: null };
+  const [functionTree, setFunctionTree] = useState<Functions.FunctionTree>([]);
+  const [originalFunctionData, setOriginalFunctionData] =
+    useState<Music.DB.TableType<"Function"> | null>(null);
   const [title, setTitle] = useState("");
   const [displayMode, setDisplayMode] = useState<DisplayMode>("blocks");
   const [code, setCode] = useState("");
+  const [isCodeValid, setIsCodeValid] = useState(true);
 
   useEffect(() => {
-    setCode(JSON.stringify(functionTree, null, 2));
-  }, [functionTree]);
+    let active = true;
+    const load = async () => {
+      if (!isNew) {
+        const functionData = await MusicLibrary.db.function.get({
+          id: oldFunctionId,
+        });
+        if (!functionData) {
+          alert("Function editor failed to load!");
+          throw new Error("Failed to load current function data!");
+        }
+        if (active) {
+          setOriginalFunctionData(functionData);
+          setTitle(functionData.title);
+          if (validateFunction(functionData.functionData)) {
+            setFunctionTree(functionData.functionData);
+          } else {
+            alert("Function data was corrupt and failed to load!");
+          }
+        }
+      }
+    };
+    load();
+    return () => {
+      active = false;
+    };
+  }, [isNew, oldFunctionId]);
 
   useEffect(() => {
-    let obj;
     try {
-      obj = JSON.parse(code);
-      setIsValid(validateFunction(obj));
+      const obj = JSON.parse(code);
+      if (validateFunction(obj)) {
+        setFunctionTree(obj);
+        setIsCodeValid(true);
+      } else {
+        setIsCodeValid(false);
+      }
     } catch (e) {
-      setIsValid(false);
+      setIsCodeValid(false);
     }
   }, [code]);
-  const [isValid, setIsValid] = useState(false);
+
+  const isFunctionValid = useMemo(
+    () => validateFunction(functionTree),
+    [functionTree]
+  );
+
+  const canSwitch = isFunctionValid && (displayMode !== "text" || isCodeValid);
+
+  const canSave = canSwitch && title.trim().length > 0;
+
+  const saveFunction = useCallback(async () => {
+    if (!canSave) {
+      alert("This function isn't saveable right now!");
+      return;
+    }
+    const toastId = toast(`${isNew ? "Creating" : "Updating"} function...`, {
+      autoClose: false,
+      isLoading: true,
+      closeButton: false,
+      closeOnClick: false,
+      draggable: false,
+      type: "info",
+    });
+    if (isNew) {
+      pages.navigate("back");
+      const newFunction = await MusicLibrary.db.function.create({
+        title,
+        functionData: functionTree as unknown as Prisma.JsonArray,
+      });
+      const success = newFunction !== null;
+
+      toast.update(toastId, {
+        render: success
+          ? `Successfully created function "${newFunction.title}"`
+          : "Failed to update function!",
+        autoClose: 5000,
+        draggable: true,
+        closeOnClick: true,
+        closeButton: true,
+        isLoading: false,
+        type: success ? "success" : "error",
+      });
+    } else {
+      if (!originalFunctionData) {
+        alert("Failed to save function!");
+        return;
+      }
+      pages.navigate("back");
+
+      const updatedFunction = await MusicLibrary.db.function.update(
+        {
+          id: originalFunctionData.id,
+        },
+        {
+          title: title,
+          functionData: functionTree as unknown as Prisma.JsonArray,
+        }
+      );
+      const success = updatedFunction !== null;
+
+      toast.update(toastId, {
+        render: (
+          <span>
+            {success
+              ? "Successfully updated function"
+              : "Failed to update function!"}
+          </span>
+        ),
+        autoClose: 5000,
+        draggable: true,
+        closeOnClick: true,
+        closeButton: true,
+        isLoading: false,
+        type: success ? "success" : "error",
+      });
+    }
+  }, [functionTree, isNew, canSave, originalFunctionData, pages, title]);
+
   return (
     <div className="flex flex-col h-full overflow-hidden">
       <div className="border-b-2 p-2 flex gap-2">
@@ -56,10 +170,12 @@ export default function FunctionEditor() {
           }}
         />
         <div className="ml-auto flex gap-2">
-          <span className="my-auto">Is Valid: {JSON.stringify(isValid)}</span>
           <button
-            disabled
-            className="group flex gap-2 items-center text-white bg-green-500 dark:invert p-2 px-4 rounded-md disabled:bg-green-700"
+            disabled={!canSave}
+            onClick={() => {
+              saveFunction();
+            }}
+            className="group flex gap-2 items-center text-white bg-green-500 dark:invert p-2 px-4 rounded-md disabled:bg-gray-700"
           >
             <span>Save</span>
             <Save className="group-disabled:hidden" />
@@ -69,17 +185,23 @@ export default function FunctionEditor() {
             {(["blocks", "text"] as DisplayMode[]).map((type) => (
               <button
                 key={type}
+                disabled={!canSwitch}
                 onClick={() => {
+                  if (type === "text") {
+                    setCode(JSON.stringify(functionTree, null, 2));
+                  }
                   setDisplayMode(type);
                 }}
                 className={classNames(
                   "p-1",
                   "px-2",
                   "border",
-                  "disabled:bg-gray-400",
-                  "disabled:dark:bg-gray-200",
+                  "disabled:border-accent-muted",
+                  "disabled:dark:border-inverted-accent-muted",
                   {
                     "bg-accent dark:bg-inverted-accent": displayMode === type,
+                    "disabled:bg-accent-muted disabled:dark:bg-inverted-accent-muted":
+                      displayMode === type,
                     "rounded-l-md": type === "blocks",
                     "rounded-r-md": type === "text",
                   }
