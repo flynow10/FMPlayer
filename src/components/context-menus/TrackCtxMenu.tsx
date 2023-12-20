c0366521f3a80b795a437477aa1bd7d18f13f7ed
Nate Wagner <nate.o.wagner@gmail.com>
2023-12-20 18:05:22 -0500

    feat: add track title to context menu

--- src/components/context-menus/TrackCtxMenu.tsx
+++ src/components/context-menus/TrackCtxMenu.tsx
@@ -1,6 +1,15 @@
+import {
+  HandlerParams,
+  Item,
+  ItemParams,
+  Menu,
+  Separator,
+} from "react-contexify-props";
+
 import { useAudioPlayer } from "@/src/hooks/use-audio-player";
+import { useDatabase } from "@/src/hooks/use-database";
 import { ContextMenuPropType } from "@/src/hooks/use-media-context";
-import { Item, ItemParams, Menu, Separator } from "react-contexify";
+import { MusicLibrary } from "@/src/music/library/music-library";
 
 type TrackCtxMenuProps = {
   pageSlug: string;
@@ -31,6 +40,12 @@ export default function TrackCtxMenu(props: TrackCtxMenuProps) {
 
   return (
     <Menu id={"track-" + props.pageSlug} theme="dark" className="dark:invert">
+      <Item disabled>
+        {(args: HandlerParams<ContextMenuPropType<"track">>) => {
+          return <TrackTitle {...args} />;
+        }}
+      </Item>
+      <Separator />
       <Item
         onClick={(event) => {
           addToQueue(event, false);
@@ -61,4 +76,25 @@ export default function TrackCtxMenu(props: TrackCtxMenuProps) {
       </Item>
     </Menu>
   );
+}
+
+// eslint-disable-next-line react/no-multi-comp
+function TrackTitle(args: HandlerParams<ContextMenuPropType<"track">>) {
+  const trackId = args.props?.trackId;
+  if (typeof trackId !== "string") {
+    alert("This context menu was not set up correctly!");
+    throw new Error("Unable to add track to queue; missing track id!");
+  }
+  const [track] = useDatabase(
+    () => {
+      return MusicLibrary.db.track.get({ id: trackId });
+    },
+    null,
+    "Track",
+    [trackId]
+  );
+  if (!track) {
+    return null;
+  }
+  return track.title;
 }
